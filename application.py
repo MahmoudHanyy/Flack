8e67f051aa7a292dc0d302a387252bebdac3a9b3
MahmoudHanyy <mahmoud.hanyy007@gmail.com>
2020-06-02 05:39:29 +0200

    Final output

--- application.py
+++ application.py
@@ -1,17 +1,18 @@
 import os
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from flask import Flask, session, render_template, request, redirect, jsonify
 from flask_session import Session
 import requests
 from functools import wraps
+from collections import deque
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "my secret key"
 socketio = SocketIO(app)
 
 logged_users = []
-
-
+channels = []
+channelsMessages = dict()
 
 def login_required(f):
     @wraps(f)
@@ -25,7 +26,7 @@ def login_required(f):
 @app.route("/")
 @login_required
 def index():
-    return render_template('index.html')
+    return render_template('index.html', channels=channels)
 
 @app.route("/signin", methods=['GET','POST'])
 def signin():
@@ -53,4 +54,79 @@ def logout():
         logged_users.remove(session.get('username'))
         session.clear()
     except: pass
-    return redirect('/')
+    return redirect('/signin')
+
+
+@app.route('/create', methods=['GET','POST'])
+def create():
+    channel = request.form.get('channel')
+    if request.method == 'POST':
+
+        if channel == '':
+            return render_template('error.html', message="Channel name can not be empty!")
+        if channel in channels:
+            return render_template('chatroom.html', channel_name=channel, channels=channels, messages=channelsMessages[channel])
+        else:
+            channels.append(channel)
+            channelsMessages[channel] = deque()
+            return redirect("/channel/"+str(channel))
+            #return render_template('chatroom.html', channel_name=channel, channels=channels)
+
+
+
+@app.route("/channel/<channel>", methods=['GET','POST'])
+@login_required
+def view(channel):
+
+    session['current_channel'] = channel
+    return render_template('chatroom.html',channel_name=channel, channels=channels, messages=channelsMessages[channel])
+
+
+
+@socketio.on("joined", namespace='/')
+def joined():
+    """ Send message to announce that user has entered the channel """
+
+    # Save current channel to join room.
+    room = session.get('current_channel')
+
+    join_room(room)
+    print('da5lt', room,session.get('username'))
+    emit('status', {
+        'userJoined': session.get('username'),
+        'channel': room,
+        'msg': session.get('username') + ' has entered the channel'},
+        room=room)
+
+@socketio.on("left", namespace='/')
+def left():
+    """ Send message to announce that user has left the channel """
+
+    room = session.get('current_channel')
+
+    leave_room(room)
+    emit('status', {
+        'msg': session.get('username') + ' has left the channel'},
+        room=room)
+
+
+@socketio.on('send message')
+def send_msg(msg, timestamp):
+    """ Receive message with timestamp and broadcast on the channel """
+
+    # Broadcast only to users on the same channel.
+    room = session.get('current_channel')
+
+    # Save 100 messages and pass them when a user joins a specific channel.
+
+    if len(channelsMessages[room]) > 100:
+        # Pop the oldest message
+        channelsMessages[room].popleft()
+    print(msg)
+    channelsMessages[room].append([timestamp, session.get('username'), msg])
+
+    emit('announce message', {
+        'user': session.get('username'),
+        'timestamp': timestamp,
+        'msg': msg},
+        room=room)
